a944fbc9e24713491f6d830632b2f064f7ba74d4
Martin Reichl <REI210086@spengergasse.at>
2022-11-04 14:56:29 +0100

    Commit

---
+++ ExCollections/ExCollection.App/Klasse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ExCollection.App
+{
+    public class Klasse
+    {
+        // TODO: Erstelle ein Property Schuelers, welches alle Schüler der Klasse in einer
+        //       Liste speichert.
+        public List<Schueler> Schuelers { get; set; } = new();
+        public string Name { get; set; }
+        public string KV { get; set; }
+
+        /// <summary>
+        /// Fügt den Schüler zur Liste hinzu und setzt das Property KlasseNavigation
+        /// des Schülers korrekt auf die aktuelle Instanz.
+        /// </summary>
+        /// <param name="s"></param>
+        public void AddSchueler(Schueler s)
+        {
+            // HIER DEN CODE EINFÜGEN
+            Schuelers.Add(s);
+            s.KlasseNavigation = this;
+        }
+    }
+}
